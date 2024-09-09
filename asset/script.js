fa5ce81d611e756807633926708ddcba6cea9c22
sraffiansyah <s.raffiansyah31@gmail.com>
2024-09-09 21:35:36 +0700

    doneee

--- asset/script.js
+++ asset/script.js
@@ -9,4 +9,23 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+
+    const navLinks = document.querySelectorAll('.nav-list a');
+    
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - 60, // Sesuaikan dengan tinggi navbar Anda
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
 });
+
